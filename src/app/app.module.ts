e51f428907dd3d77e932b5759395c97d1480d2c9
hsien <hoanplpyy@hotmail.com>
2023-02-18 13:26:36 +0800

    first npm i

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthComponent } from './auth/auth.component';
+import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeService } from './recipes/recipe.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import {StoreModule} from '@ngrx/store'
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
+
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RecipesModule,
+    StoreModule.forRoot({shoppingList:shoppingListReducer}),
+    ShoppingListModule,
+    SharedModule,
+  ],
+  providers:[
+    ShoppingListService,
+    RecipeService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,57 @@
+import { Action } from "@ngrx/store";
+import { Ingredient } from "src/app/shared/ingredient.model";
+import * as ShoppingListActions from './shopping-list.actions'
+
+export interface State {
+  ingredients:Ingredient[],
+  editedIngredient:Ingredient,
+  editedIngredientIndex:number
+}
+
+export interface AppState {
+  shoppingList:State
+}
+
+const initialState :State = {
+  ingredients: [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ],
+  editedIngredient:null,
+  editedIngredientIndex:-1
+}
+
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      }
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.payload]
+      }
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      let ingredient = state.ingredients[action.payload.index];
+      ingredient = action.payload.ingredient
+      const updatedIngredient = {
+        ...ingredient,
+        ...action.payload.ingredient
+      }
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[action.payload.index] = updatedIngredient
+      return {
+        ...state,
+        ingredients: updatedIngredients
+      }
+    case ShoppingListActions.DELETE_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter((ingredient,index)=>index!==action.payload)
+      }
+    default:
+      return state
+  }
+}
